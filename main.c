b88da8022a98b5a4be7af42aa8f1270ef8115bfd
Agustin <darkstone19@gmail.com>
2023-04-19 21:58:00 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/*
+ EJERCICIO 18 : Defina una función que devuelve la cadena de caracteres resultante de reemplazar todas las ocurrencias de un carácter por otro. Ambos caracteres son pasados por parámetro, junto a la cadena original.
+*/
 int main() {
     printf("Hello, World!\n");
     return 0;
